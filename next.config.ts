92d0af9dc97d9074c712812fb39ffd6b4bd4a9ab
omukokotech <ookoth@oberlin.edu>
2026-01-03 01:37:34 -0600

    Fix CSP headers to allow Google Fonts loading

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,19 @@ const config: NextConfig = {
   images: {
     remotePatterns: [],
   },
+  headers: async () => {
+    return [
+      {
+        source: "/:path*",
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value: "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://www.googletagmanager.com https://www.google-analytics.com; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; font-src 'self' https://fonts.gstatic.com; img-src 'self' data: https:; connect-src 'self' https:; frame-ancestors 'none'",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default config;
